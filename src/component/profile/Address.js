8f790dd89e087704b31e2c07b89f2570022be49d
Ali <lahmarali963@gmail.com>
2020-10-13 17:47:32 +0100

    last version

---
+++ src/component/profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Address() {
+    return (
+        <div>
+           <p>Tunisia Chenini Tataouine </p> 
+        </div>
+    )
+}
---
+++ src/component/profile/FullName.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function FullName() {
+    return (
+        <div>
+          <p>Ali Lahmer</p> 
+        </div>
+    )
+}
+
